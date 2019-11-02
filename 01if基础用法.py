5ca9469a489d9aebbcc6bbbc8485356ecf86cca6
Lu-dashuai <aluruihua@163.com>
2019-11-02 22:30:04 +0800

    python demo

---
+++ 01if基础用法.py
@@ -0,0 +1,19 @@
+# falg = False
+# name = input('请输入姓名：')
+# if name == 'python':
+#     falg = True
+#     print('welcome boss')
+# else:
+#     print(name)
+# age = int(input("请输入你家够的年龄："))
+# if age <= 0:
+#     print("你是在逗我把")
+# elif age == 1:
+#     print("相当于14岁的人")
+# elif age == 2:
+#     print("相当于22岁的人")
+# elif age > 2:
+#     human = 22 + (age - 2)*5
+#     print("相当与人的年龄%d"%human)
+# # 退出提示
+# input("点击enter键退出")
